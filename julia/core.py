523db27c8c52ca02eb9a456033beda4c46622729
Jake Bolewski <jakebolewski@gmail.com>
2013-11-20 22:24:20 -0500

    init commit

---
+++ julia/core.py
@@ -0,0 +1,159 @@
+"""
+Bridge Python and Julia by initializing the Julia interpreter inside Python.
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (C) 2013 The IPython and Julia Development Teams.
+#
+# Distributed under the terms of the BSD License. The full license is in
+# the file COPYING, distributed as part of this software.
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+# Stdlib
+import ctypes
+import ctypes.util
+import os
+import sys
+
+#-----------------------------------------------------------------------------
+# Classes and funtions
+#-----------------------------------------------------------------------------
+
+
+class JuliaMagicError(Exception):
+    pass
+
+
+class JuliaError(Exception):
+    pass
+
+
+class Julia(object):
+    """Implements a bridge to the Julia interpreter or library.
+    This uses the Julia PyCall module to perform type conversions and allow
+    full access to the entire Julia interpreter.
+    """
+
+    def __init__(self, init_julia=True):
+        """Create a Python object that represents a live Julia interpreter.
+
+        Parameters
+        ==========
+
+        init_julia : bool
+        If True, try to initialize the Julia interpreter. If this code is
+        being called from inside an already running Julia, the flag should be
+        passed as False so the interpreter isn't re-initialized.
+
+        Note that it is safe to call this class constructor twice in the same
+        process with `init_julia` set to True, as a global reference is kept
+        to avoid re-initializing it. The purpose of the flag is only to manage
+        situations when Julia was initialized from outside this code.
+        """
+
+        # Ugly hack to register the julia interpreter globally so we can reload
+        # this extension without trying to re-open the shared lib, which kills
+        # the python interpreter. Nasty but useful while debugging
+        if hasattr(sys, '_julia_runtime'):
+            self.api = sys._julia_runtime
+            return
+
+        if init_julia:
+            if sys.platform == "linux" or sys.platform == "linux2":
+                # *.so extension on Linux
+                jpath = '/usr/lib/julia/libjulia.so'
+            elif sys.platform == "MacOS":
+                # *.dylib extension on OSX
+                jpath = '/usr/lib/julia/libjulia.dylib'
+            else:
+                #TODO: Windows....
+                raise Exception("Windows is not supported yet")
+
+            if not os.path.exists(jpath):
+                raise ValueError("Julia library not found!")
+
+            import pdb; pdb.set_trace()
+            api = ctypes.PyDLL(jpath, ctypes.RTLD_GLOBAL)
+            api.jl_init.arg_types = [ctypes.c_char_p]
+            api.jl_init(0)
+        else:
+            # we're assuming here we're fully inside a running Julia process,
+            # so we're fishing for symbols in our own process table
+            api = ctypes.PyDLL('')
+
+        # Store the running interpreter reference so we can start using it via
+        # self.jcall
+        self.api = api
+
+        # Set the return types of some of the bridge functions in ctypes
+        # terminology
+        api.jl_eval_string.argtypes = [ctypes.c_char_p]
+        api.jl_eval_string.restype = ctypes.c_void_p
+
+        api.jl_call1.restype = ctypes.c_void_p
+        api.jl_get_field.restype = ctypes.c_void_p
+        api.jl_typeof_str.restype = ctypes.c_char_p
+        api.jl_unbox_voidpointer.restype = ctypes.py_object
+
+        if init_julia:
+            # print 'Initializing Julia PyCall module...' # dbg
+            python_exe = os.path.basename(sys.executable)
+            self.jcall("1 + 1")
+            try:
+                self.jcall('using PyCall')
+            except:
+                raise JuliaError("Julia does not have package PyCall")
+            try:
+                self.jcall('pyinitialize(C_NULL)')
+            except:
+                raise JuliaError("Failed to initialize PyCall package")
+
+        # Whether we initialized Julia or not, we MUST create at least one
+        # instance of PyObject. Since this will be needed on every call, we
+        # hold it in the Julia object itself so it can survive across
+        # reinitializations.
+        api.PyObject = self.jcall('PyObject')
+
+        # Flag process-wide that Julia is initialized and store the actual
+        # runtime interpreter, so we can reuse it across calls and module
+        # reloads.
+        sys._julia_runtime = api
+
+    def jcall(self, src):
+        """Low-level call to execute a snippet of Julia source.
+
+        This only raises an exception if Julia itself throws an error, but it
+        does NO type conversion into usable Python objects nor any memory
+        management. It should never be used for returning the result of Julia
+        expressions, only to execute statements.
+        """
+        #TODO: python3 does not like this....
+        ans = self.api.jl_eval_string(str(src))
+        if not ans:
+            raise JuliaMagicError('Exception in Julia: {}'.format(src))
+        return ans
+
+    def run(self, src):
+        """
+        Execute code in Julia, and pull some of the results back into the
+        Python namespace.
+        """
+        void_p = ctypes.c_void_p
+        if src is None:
+            return None
+        ans = self.jcall(src)
+        res = self.api.jl_call1(void_p(self.api.PyObject),
+                                void_p(ans))
+        if not res:
+            raise JuliaError('ErrorException in Julia PyObject: '
+                             '{}'.format(src))
+        boxed_obj = self.api.jl_get_field(void_p(res), 'o')
+        pyobj = self.api.jl_unbox_voidpointer(void_p(boxed_obj))
+        # make sure we incref it before returning it,
+        # since this is a borrowed reference
+        ctypes.pythonapi.Py_IncRef(ctypes.py_object(pyobj))
+        return pyobj
---
+++ julia/jlapi.py
@@ -0,0 +1,61 @@
+def locate_sysimg():
+    #TODO
+    pass
+
+
+def eval_string(ctx, code):
+    #TODO
+    pass
+
+
+def init(ctx):
+    #TODO
+    pass
+
+
+def exception_occured():
+    pass
+
+
+def typeof_str():
+    pass
+
+
+def array_eltype():
+    pass
+
+
+def array_rank():
+    pass
+
+
+def array_size():
+    pass
+
+
+def array_ptr():
+    pass
+
+
+def bytestring_ptr():
+    pass
+
+
+def call1():
+    pass
+
+
+def call2():
+    pass
+
+
+def get_field():
+    pass
+
+
+def sigatomic_begin():
+    pass
+
+
+def sigatomic_end():
+    pass
