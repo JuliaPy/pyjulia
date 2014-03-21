d5ddb95bf7879d82c2913cdab20c8e27a65f0848
Jake Bolewski <jakebolewski@gmail.com>
2014-03-20 23:21:40 -0400

    remove outdated test

--- quick_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python2 -c "import julia;j=julia.Julia();assert(j.eval('1+1') == 2)"
-echo "Python2 ok..."
-python3 -c "import julia;j=julia.Julia();assert(j.eval('1+1') == 2)"
-echo "Python3 ok..."
-
