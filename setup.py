31aa12c0cf1c58b0e38def1115d84c0ca6aac17e
stonebig <stonebig>
2014-09-13 13:13:56 +0200

    make a wheel

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 """Julia/Python bridge with IPython support.
 """
+import os
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='julia',
-      version='0.1.1',
+      version='0.1.1.4',
       description=__doc__,
       author='The Julia and IPython development teams.',
       author_email='julia@julialang.org',
