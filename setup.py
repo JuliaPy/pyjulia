a73d615cae8aa00015a50fd9b19dc90d163b7687
stonebig <stonebig34@gmail.com>
2014-09-13 17:38:12 +0200

    not uploaded to pypi by anyone yet, so no need of a version change

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 from setuptools import setup
 
 setup(name='julia',
-      version='0.1.1.4',
+      version='0.1.1',
       description=__doc__,
       author='The Julia and IPython development teams.',
       author_email='julia@julialang.org',
