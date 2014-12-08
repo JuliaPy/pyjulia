73d1214974d4637694df65ff8ec46bf73d55b51b
Bussonnier Matthias <bussonniermatthias@gmail.com>
2014-12-08 12:48:15 +0100

    Fix itgnore and setup.py file.
    Add a makefile cause python packaging sucks.

--- setup.py
+++ setup.py
@@ -2,13 +2,35 @@
 """Julia/Python bridge with IPython support.
 """
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='julia',
       version='0.1.1',
       description=__doc__,
       author='The Julia and IPython development teams.',
       author_email='julia@julialang.org',
+      license='MIT',
+      keywords='julia python',
+      classifiers=[
+        # Bug for now, I cannot upload if more than 1 classifier
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        #'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        #'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        #'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.4',
+      ],
       url='http://julialang.org',
       packages=['julia'],
      )
