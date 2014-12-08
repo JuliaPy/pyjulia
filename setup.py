a5ee1182af497bc95cb45bd34e922bc3fd552277
Bussonnier Matthias <bussonniermatthias@gmail.com>
2014-12-08 14:57:29 +0100

    re-enable multil clasifiers

--- setup.py
+++ setup.py
@@ -12,12 +12,11 @@ setup(name='julia',
       license='MIT',
       keywords='julia python',
       classifiers=[
-        # Bug for now, I cannot upload if more than 1 classifier
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        #'Development Status :: 3 - Alpha',
+        'Development Status :: 3 - Alpha',
 
         # Indicate who your project is intended for
         #'Intended Audience :: Developers',
@@ -26,10 +25,10 @@ setup(name='julia',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        #'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
       ],
       url='http://julialang.org',
       packages=['julia'],
