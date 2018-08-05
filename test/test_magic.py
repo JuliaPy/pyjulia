9122090474b4245667b6ce0ac672f509b01abb7f
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-05 10:25:41 -0700

    Test failure paths of IPython magics

--- test/test_magic.py
+++ test/test_magic.py
@@ -20,3 +20,15 @@ def test_success_cell():
     jm = get_julia_magics()
     ans = jm.julia(None, '2')
     assert ans == 2
+
+
+def test_failure_line():
+    jm = get_julia_magics()
+    ans = jm.julia('pop!([])')
+    assert ans is None
+
+
+def test_failure_cell():
+    jm = get_julia_magics()
+    ans = jm.julia(None, '1 += 1')
+    assert ans is None
