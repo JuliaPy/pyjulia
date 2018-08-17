a1f0b4b6cd8adb3f61940b5f502a14d7dbfebb6d
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-17 15:48:36 -0700

    Document with_rebuilt.ignoring

--- julia/with_rebuilt.py
+++ julia/with_rebuilt.py
@@ -51,6 +51,18 @@ def maybe_rebuild(rebuild, julia):
 
 @contextmanager
 def ignoring(sig):
+    """
+    Context manager for ignoring signal `sig`.
+
+    For example,::
+
+        with ignoring(signal.SIGINT):
+            do_something()
+
+    would ignore user's ctrl-c during ``do_something()``.  This is
+    useful when launching interactive program (in which ctrl-c is a
+    valid keybinding) from Python.
+    """
     s = signal.signal(sig, signal.SIG_IGN)
     try:
         yield
