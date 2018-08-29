361c9fe8cf5afefcf9dd31bd62ea8e66c37003dd
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-29 14:12:42 -0700

    Add _linked_libpython_windows

--- test/test_find_libpython.py
+++ test/test_find_libpython.py
@@ -1,7 +1,3 @@
-import platform
-
-import pytest
-
 from julia.find_libpython import finding_libpython, linked_libpython
 from julia.core import determine_if_statically_linked
 
@@ -18,8 +14,6 @@ def test_finding_libpython_yield_type():
 # let's just check returned type of finding_libpython.
 
 
-@pytest.mark.xfail(platform.system() == "Windows",
-                   reason="linked_libpython is not implemented for Windows")
 def test_linked_libpython():
-    if determine_if_statically_linked():
+    if not determine_if_statically_linked():
         assert linked_libpython() is not None
