2ab64e77d3ab77caa816564fbd57d5875001dab7
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-28 22:50:12 -0700

    More permissive test for finding_libpython

--- test/test_utils.py
+++ test/test_utils.py
@@ -5,6 +5,8 @@ Unit tests which can be done without loading `libjulia`.
 from julia.find_libpython import finding_libpython
 
 
-def test_smoke_finding_libpython():
+def test_finding_libpython_yield_type():
     paths = list(finding_libpython())
-    assert set(map(type, paths)) == {str}
+    assert set(map(type, paths)) <= {str}
+# In a statically linked Python executable, no paths may be found.  So
+# let's just check returned type of finding_libpython.
