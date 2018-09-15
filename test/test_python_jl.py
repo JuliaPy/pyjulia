e9ce328d88dbd47f773ea8cf73f03bbac25649e6
Takafumi Arakaki <aka.tkf@gmail.com>
2018-09-14 22:26:02 -0700

    Use shlex in test_python_jl.py

--- test/test_python_jl.py
+++ test/test_python_jl.py
@@ -1,5 +1,6 @@
 from textwrap import dedent
 import os
+import shlex
 import subprocess
 
 import pytest
@@ -13,15 +14,15 @@ PYJULIA_TEST_REBUILD = os.environ.get("PYJULIA_TEST_REBUILD", "no") == "yes"
 JULIA = os.environ.get("JULIA_EXE")
 
 
-@pytest.mark.parametrize("cli_args", [
-    ["-h"],
-    ["-i", "--help"],
-    ["--julia", "false", "-h"],
-    ["--julia", "false", "-i", "--help"],
+@pytest.mark.parametrize("args", [
+    "-h",
+    "-i --help",
+    "--julia false -h",
+    "--julia false -i --help",
 ])
-def test_help_option(cli_args, capsys):
+def test_help_option(args, capsys):
     with pytest.raises(SystemExit) as exc_info:
-        parse_pyjl_args(cli_args)
+        parse_pyjl_args(shlex.split(args))
     assert exc_info.value.code == 0
 
     captured = capsys.readouterr()
@@ -29,27 +30,27 @@ def test_help_option(cli_args, capsys):
 
 
 quick_pass_cli_args = [
-    ["-h"],
-    ["-i", "--help"],
-    ["-V"],
-    ["--version", "-c", "1/0"],
+    "-h",
+    "-i --help",
+    "-V",
+    "--version -c 1/0",
 ]
 
 
-@pytest.mark.parametrize("cli_args", quick_pass_cli_args)
-def test_cli_quick_pass(cli_args):
+@pytest.mark.parametrize("args", quick_pass_cli_args)
+def test_cli_quick_pass(args):
     subprocess.check_output(
-        ["python-jl"] + cli_args,
+        ["python-jl"] + shlex.split(args),
     )
 
 
 @pytest.mark.skipif(
     not which("false"),
     reason="false command not found")
-@pytest.mark.parametrize("cli_args", quick_pass_cli_args)
-def test_cli_quick_pass_no_julia(cli_args):
+@pytest.mark.parametrize("args", quick_pass_cli_args)
+def test_cli_quick_pass_no_julia(args):
     subprocess.check_output(
-        ["python-jl", "--julia", "false"] + cli_args,
+        ["python-jl", "--julia", "false"] + shlex.split(args),
     )
 
 
@@ -60,7 +61,7 @@ def test_cli_quick_pass_no_julia(cli_args):
     not PYJULIA_TEST_REBUILD,
     reason="PYJULIA_TEST_REBUILD=yes is not set")
 def test_cli_import():
-    cli_args = ["-c", dedent("""
+    args = ["-c", dedent("""
     from julia import Base
     Base.banner()
     from julia import Main
@@ -68,9 +69,9 @@ def test_cli_import():
     assert Main.x == 1
     """)]
     if JULIA:
-        cli_args = ["--julia", JULIA] + cli_args
+        args = ["--julia", JULIA] + args
     output = subprocess.check_output(
-        ["python-jl"] + cli_args,
+        ["python-jl"] + args,
         universal_newlines=True)
     assert "julialang.org" in output
 
