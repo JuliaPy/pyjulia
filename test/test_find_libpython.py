50ad7fa1ead6375d5a7e1083d45c26c9d66c6f36
Takafumi Arakaki <aka.tkf@gmail.com>
2018-10-25 00:59:10 -0700

    Handle statically linked case in macOS as well
    It turned out macOS can have statically linked Python when it's
    installed via conda:
    https://github.com/JuliaPy/pyjulia/issues/150#issuecomment-432912833
    
    So it seems `linked_libpython` (which calls `libdl`) is the only way
    to reliably detect if the Python executable is statically linked or
    not.  Since cd2e4089c06c0945bbc4873f8dac6edf6b9cea44 implements it for
    Windows as well, we can now rely on `linked_libpython` everywhere
    which simplifies core.py.

--- test/test_find_libpython.py
+++ test/test_find_libpython.py
@@ -1,5 +1,7 @@
+import subprocess
+import sys
+
 from julia.find_libpython import finding_libpython, linked_libpython
-from julia.core import determine_if_statically_linked
 
 try:
     unicode
@@ -14,6 +16,22 @@ def test_finding_libpython_yield_type():
 # let's just check returned type of finding_libpython.
 
 
+def determine_if_statically_linked():
+    """Determines if this python executable is statically linked"""
+    if not sys.platform.startswith('linux'):
+        # Assuming that Windows and OS X are generally always
+        # dynamically linked.  Note that this is not the case in
+        # Python installed via conda:
+        # https://github.com/JuliaPy/pyjulia/issues/150#issuecomment-432912833
+        # However, since we do not use conda in our CI, this function
+        # is OK to use in tests.
+        return False
+    lddoutput = subprocess.check_output(["ldd", sys.executable])
+    return not (b"libpython" in lddoutput)
+
+
 def test_linked_libpython():
+    # TODO: Special-case conda (check `sys.version`).  See the above
+    # comments in `determine_if_statically_linked.
     if not determine_if_statically_linked():
         assert linked_libpython() is not None
