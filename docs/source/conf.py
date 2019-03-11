498288fadde4a8ad13a0977dbb2d5febd13133d1
Takafumi Arakaki <aka.tkf@gmail.com>
2019-03-11 20:47:18 +0900

    Fix `version` and `release` in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, The Julia and IPython development teams'
 author = 'The Julia and IPython development teams'
 
 # The short X.Y version
-version = ''
+version = '0.2.1'
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.2.1.dev'
 
 
 # -- General configuration ---------------------------------------------------
