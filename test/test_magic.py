e574f05329172b1ca42ae4e17f206ef15d296d89
Takafumi Arakaki <aka.tkf@gmail.com>
2019-03-12 11:39:13 +0900

    Use pytest.fixture in test_magic.py
    This is required for `--no-julia` to work.

--- test/test_magic.py
+++ test/test_magic.py
@@ -1,34 +1,32 @@
 from IPython.testing.globalipapp import get_ipython
-import julia.magic
+from julia import magic
+import pytest
 
 
-def get_julia_magics():
-    return julia.magic.JuliaMagics(shell=get_ipython())
+@pytest.fixture
+def julia_magics(julia):
+    return magic.JuliaMagics(shell=get_ipython())
 
 
-def test_register_magics():
-    julia.magic.load_ipython_extension(get_ipython())
+def test_register_magics(julia):
+    magic.load_ipython_extension(get_ipython())
 
 
-def test_success_line():
-    jm = get_julia_magics()
-    ans = jm.julia('1')
+def test_success_line(julia_magics):
+    ans = julia_magics.julia('1')
     assert ans == 1
 
 
-def test_success_cell():
-    jm = get_julia_magics()
-    ans = jm.julia(None, '2')
+def test_success_cell(julia_magics):
+    ans = julia_magics.julia(None, '2')
     assert ans == 2
 
 
-def test_failure_line():
-    jm = get_julia_magics()
-    ans = jm.julia('pop!([])')
+def test_failure_line(julia_magics):
+    ans = julia_magics.julia('pop!([])')
     assert ans is None
 
 
-def test_failure_cell():
-    jm = get_julia_magics()
-    ans = jm.julia(None, '1 += 1')
+def test_failure_cell(julia_magics):
+    ans = julia_magics.julia(None, '1 += 1')
     assert ans is None
