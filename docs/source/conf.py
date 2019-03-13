8e241d4e62043b53feba3b888e93e0c7bfb98437
Takafumi Arakaki <aka.tkf@gmail.com>
2019-03-13 20:00:20 +0900

    Bump to 0.3.0.dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, The Julia and IPython development teams'
 author = 'The Julia and IPython development teams'
 
 # The short X.Y version
-version = '0.2.1'
+version = '0.3.0'
 # The full version, including alpha/beta/rc tags
-release = '0.2.1.dev'
+release = '0.3.0.dev'
 
 
 # -- General configuration ---------------------------------------------------
