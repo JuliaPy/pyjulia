18b66119586a97b9ed4486721915f5dcdfafdc26
Takafumi Arakaki <aka.tkf@gmail.com>
2019-03-25 16:23:20 -0700

    Move conftest.py to top-level

--- conftest.py
+++ conftest.py
@@ -1,8 +1,8 @@
 import sys
 
 if sys.version_info[0] < 3:
-    collect_ignore = [
-        "monkeypatch_completer.py",
-        "monkeypatch_interactiveshell.py",
+    collect_ignore_glob = [
+        "**/monkeypatch_completer.py",
+        "**/monkeypatch_interactiveshell.py",
     ]
     # Theses files are ignored as import fails at collection phase.
