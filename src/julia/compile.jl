60dca9ea3c1dc702f8a13855924c471a3b0b2bff
Takafumi Arakaki <aka.tkf@gmail.com>
2019-04-10 22:24:41 -0700

    Automatically install PackageCompiler in a private environment

--- src/julia/compile.jl
+++ src/julia/compile.jl
@@ -1,12 +1,22 @@
 script, output = ARGS
 
 using Pkg
+
+compilerenv = abspath("compilerenv")
+Pkg.activate(compilerenv)
+Pkg.add([
+    PackageSpec(
+        name = "PackageCompiler",
+        uuid = "9b87118b-4619-50d2-8e1e-99f35a4d4d9d",
+        version = "0.6",
+    )
+])
+using PackageCompiler
+
 Pkg.activate(".")
 Pkg.add("MacroTools")
 Pkg.add("PyCall")
-Pkg.activate()
 
-using PackageCompiler
 sysout, _curr_syso = compile_incremental("Project.toml", script)
 
 cp(sysout, output, force=true)
