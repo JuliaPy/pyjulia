09446b0f02945f6d223655e109b33e4fbfdfef0d
Takafumi Arakaki <aka.tkf@gmail.com>
2019-04-11 02:06:11 -0700

    xfail

--- src/julia/tests/test_runtests.py
+++ src/julia/tests/test_runtests.py
@@ -7,6 +7,10 @@ from julia.core import _enviorn
 from .test_compatible_exe import run
 
 
+def test_THIS_TEST_MUST_FAIL():
+    assert False
+
+
 def test_runtests_failure(tmp_path):
     testfile = tmp_path / "test.py"
     testcode = u"""
