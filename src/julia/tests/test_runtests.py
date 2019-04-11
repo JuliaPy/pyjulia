420ec092626d72ea465ee33115ff6635b39f0cd3
Takafumi Arakaki <aka.tkf@gmail.com>
2019-04-11 00:56:28 -0700

    Test julia.runtests can fail properly

---
+++ src/julia/tests/test_runtests.py
@@ -0,0 +1,35 @@
+import subprocess
+import sys
+from textwrap import dedent
+
+from julia.core import _enviorn
+
+from .test_compatible_exe import run
+
+
+def test_runtests_failure(tmp_path):
+    testfile = tmp_path / "test.py"
+    testcode = u"""
+    def test_THIS_TEST_MUST_FAIL():
+        assert False
+    """
+    testfile.write_text(dedent(testcode))
+
+    proc = run(
+        [
+            sys.executable,
+            "-m",
+            "julia.runtests",
+            "--",
+            str(testfile),
+            "--no-julia",
+            "-k",
+            "test_THIS_TEST_MUST_FAIL",
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+        env=_enviorn,
+    )
+    assert proc.returncode == 1
+    assert "1 failed" in proc.stdout
