015732378117767c5baf2ba6f731fb5bcee7fa60
Marius Millea <mariusmillea@gmail.com>
2019-09-11 19:31:03 -0700

    honor trailing semicolon in Julia magic to suppress return value (#320)
    * honor trailing semicolon in Julia magic to suppress return value
    
    * dont return value from Julia at all if suppressed, fix test
    
    * Use black style

--- src/julia/magic.py
+++ src/julia/magic.py
@@ -105,21 +105,25 @@ class JuliaMagics(Magics):
         """
         src = compat.unicode_type(line if cell is None else cell)
 
-        # fmt: off
-
         # We assume the caller's frame is the first parent frame not in the
         # IPython module. This seems to work with IPython back to ~v5, and
         # is at least somewhat immune to future IPython internals changes,
         # although by no means guaranteed to be perfect.
         caller_frame = sys._getframe(3)
-        while caller_frame.f_globals.get('__name__').startswith("IPython"):
+        while caller_frame.f_globals.get("__name__").startswith("IPython"):
             caller_frame = caller_frame.f_back
-        
-        return self._julia.eval("""
-        _PyJuliaHelper.@prepare_for_pyjulia_call begin %s end
-        """%src)(self.shell.user_ns, caller_frame.f_locals)
 
-        # fmt: on
+        return_value = "nothing" if src.strip().endswith(";") else ""
+
+        return self._julia.eval(
+            """
+            _PyJuliaHelper.@prepare_for_pyjulia_call begin
+                begin %s end
+                %s
+            end
+            """
+            % (src, return_value)
+        )(self.shell.user_ns, caller_frame.f_locals)
 
 
 # Add to the global docstring the class information.
--- src/julia/tests/test_magic.py
+++ src/julia/tests/test_magic.py
@@ -133,6 +133,13 @@ def test_global_scope(run_cell):
         return ret
     f()
     """) == "global"
+    
+def test_noretvalue(run_cell):
+    assert run_cell("""
+    %%julia
+    1+2;
+    """) is None
+
 
 def test_revise_error():
     from julia.ipy import revise
