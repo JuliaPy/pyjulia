31c09b910e4d7eea80c15ae5d7136ad4d73939a1
Takafumi Arakaki <aka.tkf@gmail.com>
2020-04-25 15:22:20 -0700

    Use `Base.require` instead of `using` for robustly loading stdlibs (#377)

--- src/julia/compile.jl
+++ src/julia/compile.jl
@@ -4,7 +4,8 @@ if VERSION < v"0.7-"
     error("Unsupported Julia version $VERSION")
 end
 
-using Pkg
+const Pkg =
+    Base.require(Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg"))
 
 Pkg.activate(compiler_env)
 @info "Loading PackageCompiler..."
@@ -13,7 +14,7 @@ using PackageCompiler
 @info "Installing PyCall..."
 Pkg.activate(".")
 Pkg.add([
-    PackageSpec(
+    Pkg.PackageSpec(
         name = "PyCall",
         uuid = "438e738f-606a-5dbb-bf0a-cddfbfd45ab0",
     ),
