a544cd911175ab21b3d1a2fd0ff5ac601e52baf5
Takafumi Arakaki <aka.tkf@gmail.com>
2020-04-24 19:24:56 -0700

    Bump to 0.5.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'The Julia and IPython development teams'
 # The short X.Y version
 version = '0.5.2'
 # The full version, including alpha/beta/rc tags
-release = '0.5.2.dev'
+release = '0.5.2'
 
 
 # -- General configuration ---------------------------------------------------
