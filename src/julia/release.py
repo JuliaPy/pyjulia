15504cddfefb477048aa09c17e9ed6fde65ca037
Takafumi Arakaki <aka.tkf@gmail.com>
2020-04-25 15:26:56 -0700

    Bump to 0.5.4.dev

--- src/julia/release.py
+++ src/julia/release.py
@@ -1,5 +1,5 @@
 # This file is executed via setup.py and imported via __init__.py
 
-__version__ = "0.5.3"
+__version__ = "0.5.4.dev"
 # For Python versioning scheme, see:
 # https://www.python.org/dev/peps/pep-0440/#version-scheme
