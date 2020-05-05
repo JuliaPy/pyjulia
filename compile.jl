0dde27b70f53f9a64557937cdfeb7fe1a5debe93
grahamgill <47540120+grahamgill@users.noreply.github.com>
2020-05-04 22:00:24 -0400

    remove debug statements

---
+++ compile.jl
@@ -0,0 +1,32 @@
+compiler_env, script, output, base_sysimage = ARGS
+
+if VERSION < v"0.7-"
+    error("Unsupported Julia version $VERSION")
+end
+
+const Pkg =
+    Base.require(Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg"))
+
+Pkg.activate(compiler_env)
+@info "Loading PackageCompiler..."
+using PackageCompiler
+
+@info "Installing PyCall..."
+Pkg.activate(".")
+Pkg.add([
+    Pkg.PackageSpec(
+        name = "PyCall",
+        uuid = "438e738f-606a-5dbb-bf0a-cddfbfd45ab0",
+    ),
+])
+
+@info "Compiling system image..."
+create_sysimage(
+    [:PyCall],
+    sysimage_path = output,
+    project = ".",
+    precompile_execution_file = script,
+    base_sysimage = isempty(base_sysimage) ? nothing : base_sysimage,
+)
+
+@info "System image is created at $output"
