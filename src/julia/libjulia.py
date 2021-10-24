7ae969807d52c91189a8b79e0baafe546256ac96
Takafumi Arakaki <aka.tkf@gmail.com>
2021-10-24 01:05:03 -0400

    Ignore when jl_ is not defined

--- src/julia/libjulia.py
+++ src/julia/libjulia.py
@@ -31,8 +31,13 @@ UNBOXABLE_TYPES = (
 
 def setup_libjulia(libjulia):
     # Store the running interpreter reference so we can start using it via self.call
-    libjulia.jl_.argtypes = [c_void_p]
-    libjulia.jl_.restype = None
+    try:
+        jl_ = libjulia.jl_
+    except AttributeError:
+        pass
+    else:
+        jl_.argtypes = [c_void_p]
+        jl_.restype = None
 
     # Set the return types of some of the bridge functions in ctypes terminology
     libjulia.jl_eval_string.argtypes = [c_char_p]
