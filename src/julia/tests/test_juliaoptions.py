939d2149c468269b836486a4b59ad006641fa8d2
MilesCranmer <miles.cranmer@gmail.com>
2022-11-09 13:59:09 -0500

    Fix options test

--- src/julia/tests/test_juliaoptions.py
+++ src/julia/tests/test_juliaoptions.py
@@ -7,13 +7,13 @@ from julia.core import JuliaOptions
 @pytest.mark.parametrize("kwargs, args", [
     ({}, []),
     (dict(compiled_modules=None), []),
-    (dict(compiled_modules=False), ["--compiled-modules", "no"]),
-    (dict(compiled_modules="no"), ["--compiled-modules", "no"]),
-    (dict(depwarn="error"), ["--depwarn", "error"]),
-    (dict(sysimage="PATH"), ["--sysimage", "PATH"]),
-    (dict(optimize=3), ["--optimize", "3"]),
-    (dict(threads=4), ["--threads", "4"]),
-    (dict(min_optlevel=2), ["--min-optlevel", "2"]),
+    (dict(compiled_modules=False), ["--compiled-modules=no"]),
+    (dict(compiled_modules="no"), ["--compiled-modules=no"]),
+    (dict(depwarn="error"), ["--depwarn=error"]),
+    (dict(sysimage="PATH"), ["--sysimage=PATH"]),
+    (dict(optimize=3), ["--optimize=3"]),
+    (dict(threads=4), ["--threads=4"]),
+    (dict(min_optlevel=2), ["--min-optlevel=2"]),
 ])
 # fmt: on
 def test_as_args(kwargs, args):
