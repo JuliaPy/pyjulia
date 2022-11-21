58842ab95f05477b24f9d78dcfb2bca22bc80c6f
Mark Kittisopikul <mkitti@users.noreply.github.com>
2022-11-20 20:48:16 -0500

    Bump to 0.6.0
    * Bump to 0.6.0
    
    Co-authored-by: Miles Cranmer <miles.cranmer@gmail.com>

--- src/julia/release.py
+++ src/julia/release.py
@@ -1,5 +1,5 @@
 # This file is executed via setup.py and imported via __init__.py
 
-__version__ = "0.5.8.dev"
+__version__ = "0.6.0"
 # For Python versioning scheme, see:
 # https://www.python.org/dev/peps/pep-0440/#version-scheme
