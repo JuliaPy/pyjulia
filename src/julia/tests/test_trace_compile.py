fdd5fd25a2477495b2d2d26bc55a9b47c4e27810
Robert Dyro <rdyro@stanford.edu>
2023-05-07 11:19:50 -0700

    hopefully fixing the trace compile file creating via full path specification

--- src/julia/tests/test_trace_compile.py
+++ src/julia/tests/test_trace_compile.py
@@ -8,16 +8,17 @@ from .test_compatible_exe import runcode
 
 
 def test_trace_file_created(tmpdir):
-    currdir = os.getcwd()
-    os.chdir(str(tmpdir))
+    trace_compile_path = Path(tmpdir) / "trace_compile.jl"
     runcode(
         """
     from julia.api import Julia
-    jl = Julia(trace_compile="trace_compile.jl")
+    jl = Julia(trace_compile="{}")
 
     from julia import Main
     Main.sin(2.0)
-    """
+    """.format(
+            str(trace_compile_path)
+        )
     )
-    os.chdir(currdir)
-    assert (Path(tmpdir) / "trace_compile.jl").exists()
+    assert (trace_compile_path).exists()
+    assert len(trace_compile_path.read_text().strip()) > 0
