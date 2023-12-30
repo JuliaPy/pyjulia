de79d3a7a963fa8d6c7cf884d45a819ba370577a
MilesCranmer <miles.cranmer@gmail.com>
2023-12-30 02:36:58 +0000

    Bump to 0.6.2
    Squashed commit of the following:
    
    commit 18589bbbe7db720e8b8f83742134557937d8b7f5
    Author: MilesCranmer <miles.cranmer@gmail.com>
    Date:   Sat Dec 30 02:25:30 2023 +0000
    
        Bump to 0.6.2
    
        Squashed commit of the following:
    
        commit 12cba0885fe947ca6a3e0cb8d29dc119ac4d9787
        Merge: 28af425 ce3ac83
        Author: Miles Cranmer <miles.cranmer@gmail.com>
        Date:   Sat Dec 30 02:23:21 2023 +0000
    
            Merge pull request #544 from JuliaPy/update-to-0.6.2
    
            Bump to 0.6.2 with Python 3.12 fix
    
        commit ce3ac83901ae4a702215a63860b8fd469a3cdf52
        Author: MilesCranmer <miles.cranmer@gmail.com>
        Date:   Sat Dec 30 02:03:45 2023 +0000
    
            Bump to 0.6.2 with Python 3.12 fix
    
        commit 28af4251bb3ec92d6a87c69cc77f1556238eb58e
        Author: Miles Cranmer <miles.cranmer@gmail.com>
        Date:   Fri Dec 29 16:55:50 2023 +0000
    
            Update setup actions and get working on 3.12 (#541)
    
            * Update setup actions
    
            * Fix PyJulia on Python 3.12 (#538)
    
            * Fix importlib deprecated API
    
            * Test Python 3.12
    
            * Test Julia 1.9
    
            * Fix semantics of exec_module vs create_module
    
            * Ensure we can find spec from JuliaModule
    
            * Bump version with python 3.12 compat
    
            * Declare compat for 3.11 and 3.12
    
            * Only test 1.6, 1.9, 1

--- src/julia/release.py
+++ src/julia/release.py
@@ -1,5 +1,5 @@
 # This file is executed via setup.py and imported via __init__.py
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 # For Python versioning scheme, see:
 # https://www.python.org/dev/peps/pep-0440/#version-scheme
