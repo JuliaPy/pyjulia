f0981f2512c7ebbfdf8ff5759f9dedde02d66982
root <rdyro@stanford.edu>
2024-01-17 15:08:55 -0800

    adding expected precompile call test

--- src/julia/tests/test_trace_compile.py
+++ src/julia/tests/test_trace_compile.py
@@ -1,8 +1,5 @@
-import os
+import re
 from pathlib import Path
-from subprocess import check_call
-
-import pytest
 
 from .test_compatible_exe import runcode
 from .utils import skip_in_windows
@@ -12,9 +9,9 @@ from .utils import skip_in_windows
 def test_trace_file_created(tmpdir):
     trace_compile_path = Path(tmpdir) / "trace_compile.jl"
     runcode(
-        """
+        f"""
     from julia.api import Julia
-    jl = Julia(trace_compile="{}")
+    jl = Julia(trace_compile="{trace_compile_path}")
 
     from julia import Main
     Main.sin(2.0)
@@ -24,3 +21,9 @@ def test_trace_file_created(tmpdir):
     )
     assert (trace_compile_path).exists()
     assert len(trace_compile_path.read_text().strip()) > 0
+
+    # check whether the sin precompilation directive is included in the file
+    trace_compile_content = Path(trace_compile_path).read_text().strip()
+    lines = [x for x in trace_compile_content.split("\n") if len(x) > 0]
+    expected_precompile_line = r"precompile\(Tuple\{typeof\([A-Za-z]+\.sin\), Float64\}\)"
+    assert any([re.match(expected_precompile_line, x) is not None for x in lines])
